0f05d1bfcc508f6b73435d1ad8802093c7b42b86
Pavel <pasha1201s@inbox.ru>
2022-07-05 03:06:01 +0300

    edit config.ts

--- config.ts
+++ config.ts
@@ -1,14 +1,14 @@
 
 export default {
         DEBUG: true,
-        API_SERVER: String(process.env.API_SERVER) || 'http://localhost:9000' ,
-        API_PORT: Number(process.env.API_PORT)|| 9000,
-        CLIENT_SERVER: String( process.env.CLIENT_SERVER) || 'http://localhost:8080',
-        DB_USER: String(process.env.DB_USERNAME) || 'shop',
-        DB_PASSWORD: String( process.env.DB_PASSWORD) || 'shop',
-        DB_HOST: String(process.env.DB_HOST) || 'localhost',
-        DB_NAME: String(process.env.DB_NAME) || 'shop_db',
-        JWT_KEY:  String(process.env.JWT_KEY) || 'veryshortkey',
-        ADMIN_EMAIL: String(process.env.ADMIN_EMAIL) || 'test@mail.ru',
-        ADMIN_PASSWORD: String(process.env.ADMIN_PASSWORD) || 'administrator'
+        API_SERVER: process.env.API_SERVER || 'http://localhost:9000' ,
+        API_PORT: process.env.API_PORT || 9000,
+        CLIENT_SERVER:  process.env.CLIENT_SERVER || 'http://localhost:8080',
+        DB_USER: process.env.DB_USERNAME || 'shop',
+        DB_PASSWORD:  process.env.DB_PASSWORD || 'shop',
+        DB_HOST: process.env.DB_HOST || 'localhost',
+        DB_NAME: process.env.DB_NAME || 'shop_db',
+        JWT_KEY:  process.env.JWT_KEY || 'veryshortkey',
+        ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'test@mail.ru',
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'administrator'
     }
